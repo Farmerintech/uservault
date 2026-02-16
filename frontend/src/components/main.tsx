50766b8e53589ec2bbe57c871a7c9e5fbd8806d1
farmerintech <farmerintech@gmail.com>
2026-02-16 10:51:06 +0100

    reset psw

--- frontend/src/components/main.tsx
+++ frontend/src/components/main.tsx
@@ -28,7 +28,7 @@ export const Main = () =>{
 
     return (
         <main className="flex flex-col gap-5 lg:w-[50%] md:w-[40%] p-5 md:pt-5 relative lg:left-[22%] md:left-[32%]">
-            
+            <p>{msg && msg}</p>
         </main>
     )
 }
