670b44d2a8add6e6ee241668e7d59b716b1228ca
farmerintech <farmerintech@gmail.com>
2026-02-16 11:26:28 +0100

    corrcet error

--- frontend/src/components/api.tsx
+++ frontend/src/components/api.tsx
@@ -1,3 +1,5 @@
-export const BaseURL = `https://uservault-qo2f.onrender.com/api/v1`
+export const BaseURL = `http://localhost:8000/api/v1`
+
+// `https://uservault-qo2f.onrender.com/api/v1`
 
 // `http://localhost:8000/api/v1`
