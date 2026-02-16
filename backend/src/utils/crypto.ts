a7551b731be5b50888479d941a94cceca92a9aaa
farmerintech <farmerintech@gmail.com>
2026-02-16 11:12:16 +0100

    corrcet error

--- backend/src/utils/crypto.ts
+++ backend/src/utils/crypto.ts
@@ -2,43 +2,30 @@ import crypto from "crypto";
 
 const algorithm = "aes-256-cbc";
 
-// Must be 32 bytes
-const secretKey = Buffer.from(
-  process.env.ENCRYPT_SECRET || "",
-  "utf-8"
-);
+// Convert from hex to Buffer (32 bytes)
+const secretKey = Buffer.from(process.env.ENCRYPT_SECRET || "", "hex");
 
-// Must be 16 bytes
+if (!secretKey || secretKey.length !== 32) {
+  throw new Error("Invalid ENCRYPT_SECRET length (must be 32 bytes)");
+}
+
 const ivLength = 16;
-console.log(process.env.ENCRYPT_SECRET)
+
 /* ================= ENCRYPT ================= */
 export const encryptData = (text: string): string => {
-  if (!secretKey) {
-    throw new Error("Invalid ENCRYPT_SECRET length (must be 32 chars)");
-  }
-
   const iv = crypto.randomBytes(ivLength);
-
   const cipher = crypto.createCipheriv(algorithm, secretKey, iv);
 
   let encrypted = cipher.update(text, "utf8", "hex");
   encrypted += cipher.final("hex");
 
-  // iv + encrypted data
   return iv.toString("hex") + ":" + encrypted;
 };
 
 /* ================= DECRYPT ================= */
 export const decryptData = (encryptedText: string): string => {
-  if (!secretKey || secretKey.length !== 32) {
-    throw new Error("Invalid ENCRYPT_SECRET length (must be 32 chars)");
-  }
-
   const parts = encryptedText.split(":");
-
-  if (parts.length !== 2) {
-    throw new Error("Invalid encrypted format");
-  }
+  if (parts.length !== 2) throw new Error("Invalid encrypted format");
 
   const iv = Buffer.from(parts[0], "hex");
   const encrypted = parts[1];
