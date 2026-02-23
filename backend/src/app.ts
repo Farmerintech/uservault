95c6ee3b26f43d467a4e5e776f5279c8ef56a058
farmerintech <farmerintech@gmail.com>
2026-02-23 11:31:14 +0100

    added cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,7 +8,27 @@ import FileRoute from "./routes/fileRoutes";
 import userRoute from "./routes/userRoutes";
 
 const app = express();
-app.use(cors())
+
+
+// Allow requests from specific origins
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://securevault.citadel-i.com.ng'
+];
+
+app.use(cors({
+  origin: function(origin, callback) {
+    // Allow requests with no origin (like mobile apps or curl)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = `The CORS policy for this site does not allow access from the specified Origin.`;
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  methods: ['GET','POST','PUT','DELETE','OPTIONS'],
+  credentials: true, // If you need to allow cookies/auth headers
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
