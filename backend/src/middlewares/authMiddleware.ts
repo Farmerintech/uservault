8fcd5ae5f577fe09a6b9d95acbb6bfe461b73fef
farmerintech <farmerintech@gmail.com>
2026-02-25 09:51:30 +0100

    orrected login flow

--- backend/src/middlewares/authMiddleware.ts
+++ backend/src/middlewares/authMiddleware.ts
@@ -5,7 +5,7 @@ import jwt from "jsonwebtoken";
 export interface AuthRequest extends Request {
   user?: any;
 }
-export const protect = (req: AuthRequest, res: Response, next: NextFunction) => {
+export const authMiddlewar = (req: AuthRequest, res: Response, next: NextFunction) => {
   try {
     const token = req.headers.authorization?.split(" ")[1];
 
